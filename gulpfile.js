d597332dda276ea3da48d952a251c7728df52a66
Fabian Beiner <fb@fabianbeiner.de>
2015-09-17 22:35:00 +0200

    Added “autoprefixer” and “gulp-postcss” to package.json and used both in the gulp process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 
 gulp.task('default', function () {
-	var autoprefixer = require('autoprefixer-core');
+	var autoprefixer = require('autoprefixer');
 	var minifyCSS    = require('gulp-minify-css');
 	var postcss      = require('gulp-postcss');
 	var sass         = require('gulp-sass');
 	var sourcemaps   = require('gulp-sourcemaps');
+	var processors = [
+		autoprefixer({browsers: ["Android >= 4.3", "Chrome >= 39", "Edge >= 12", "Firefox >= 34", "Explorer >= 9", "iOS >= 7", "Safari >= 7", "ExplorerMobile >= 10"]})
+	];
 
 	return gulp.src('sanitize.scss')
 		.pipe(sourcemaps.init())
@@ -15,6 +18,7 @@ gulp.task('default', function () {
 		.pipe(minifyCSS({
 			keepBreaks: true
 		}))
+		.pipe(postcss(processors))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./dist'));
 });
